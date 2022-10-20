0e054246cdb393e2dd8cee87b7ce30197f5e5c32
Eugene Park <eugenepark03@gmail.com>
2022-10-19 20:50:16 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/eugenepark404%40gmail.com_dealership/guestbook/read-guestbook-entries';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/eugenepark404%40gmail.com_dealership/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
